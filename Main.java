418a19e11cfce6fc9af916856e66cf011f0841fd
BickeyManandhar <manandharbickey@gmail.com>
2023-11-05 12:06:46 -0500

    second commit

--- Main.java
+++ Main.java
@@ -2,7 +2,8 @@ import java.util.Date;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Time and Date:"+new Date());
+    System.out.println("Time and Date:" + new Date());
     System.out.println("This is output from Java program.");
+    System.out.println("Adding one more line to see if auto build is working.");
   }
 }
