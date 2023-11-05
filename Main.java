39bea0ed54d545e05c9ccd3e7921f97909c0027e
BickeyManandhar <manandharbickey@gmail.com>
2023-11-05 11:53:26 -0500

    first commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Time and Date:"+new Date());
+    System.out.println("This is output from Java program.");
+  }
+}
